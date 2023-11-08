74516022f9823bf5ce5bc0351dad4c8e7898b3ce
Taven Tran <taventran@gmail.com>
2023-11-07 21:28:10 -0600

    First push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './pfp2.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="font-mono">
+      <header className="text-turqoise font-bold text-2xl grid grid-cols-9 mt-[2%] ml-[2%]">
+        <h1 className="hover:text-gold hover:cursor-pointer">About Me</h1>
+        <h1 className="hover:text-gold hover:cursor-pointer">Projects</h1>
+        <h1 className="hover:text-gold hover:cursor-pointer">Socials</h1>
       </header>
+      <hr className="text-gold border-[3px] w-[96%] ml-[2%] my-[2%]"></hr>
+      <body className="text-center">
+        <img
+          src={logo}
+          alt="Logo"
+          className="mt-[1%] w-[18%] h-[18%] border-[5px] border-gold rounded-full mx-auto"
+        />
+        <h2 className="text-gold mt-4 text-3xl">
+          Hello, I’m Taven an aspiring software engineer. With previous intern
+          experience at Halliburton and JP Morgan & Chase. I hope to keep
+          growing in this field and to make a positive impact where ever I am.
+        </h2>
+      </body>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      turqoise: "#178582",
+      gold: "#BFA181",
+    },
+  },
+  plugins: [],
+};
+
